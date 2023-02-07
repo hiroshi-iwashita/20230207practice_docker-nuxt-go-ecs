f37dc95818bb45985892242b68e9f42295c2f91f
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2023-02-07 12:29:00 +0900

    update: ESLintの設定を修正

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
     "env": {
-        "browser": true,
         "es2021": true,
         "node": true
     },
     "extends": [
         "eslint:recommended",
         "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "@nuxtjs/eslint-config-typescript"
     ],
     "overrides": [
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": "latest",
+        "ecmaVersion": 13,
+        "parser": "@typescript-eslint/parser",
         "sourceType": "module"
     },
     "plugins": [
