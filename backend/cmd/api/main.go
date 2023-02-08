7b44e6a32105583e1f29eaccdd00b8e42410571f
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2023-02-08 10:17:15 +0900

    add: main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,6 +5,19 @@ import (
 	"runtime"
 )
 
+// application is the type for all data we want to share with the
+// various parts of our application. We will share this information
+// in most cases by using this type as the receiver for functions.
+// type applicationConfig struct {
+// 	port         int
+// 	infoLog      *log.Logger
+// 	errorLog     *log.Logger
+// 	// models       models.Models
+// 	environment  string
+// 	inProduction bool
+// }
+
+
 func main() {
 	fmt.Println("Hello Golang world.")
 	fmt.Println("Go version :", runtime.Version(), ", OS :", runtime.GOOS, ", ARCH :", runtime.GOARCH)
