8abe81e6af816ae9b90f32a521965298def95ffd
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2023-02-09 12:27:13 +0900

    add: runtimeConfigの設定

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    app: {
+      baseURL: "/",
+      buildAssetsDir: "/_nuxt/",
+      cdnURL: "",
+    },
+    public: {
+      baseURL: "",
+    },
+    apiBaseURL: "",
+  },
   typescript: {
     strict: true,
   },
